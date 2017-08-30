71d1dd8f3d893775650bb32d5195801a0711818e
coacoacoa <coacoacoa@gmail.com>
2017-08-31 00:05:18 +0200

    fixes

--- src/Provider/ValueObjects/CustomerPhone.php
+++ src/Provider/ValueObjects/CustomerPhone.php
@@ -4,9 +4,8 @@ namespace League\OAuth2\Client\Provider\ValueObjects;
 
 class CustomerPhone
 {
-    public $created_by_customer;
+    public $customer_phone_id;
     public $customer_id;
     public $phone_number;
-    public $name_prefix;
     public $name;
 }
