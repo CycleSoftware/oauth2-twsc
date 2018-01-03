296de2cd379e68ac9a0abb6116e7d178b41b7e2e
coacoacoa <coacoacoa@gmail.com>
2018-01-03 09:57:28 +0100

    add rating class

---
+++ src/Provider/ValueObjects/Rating.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace League\OAuth2\Client\Provider\ValueObjects;
+
+class Rating
+{
+    public $customer_token;
+    public $account_id;
+    public $store_id;
+    public $customer_id;
+    public $entity_type_id;
+    public $entity_id;
+    public $five_stars;
+    public $contact_request;
+    public $description;
+    public $employee_id;
+    public $json_meta_data;
+}
